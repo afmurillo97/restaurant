664c495175c56a03900cabeaaa289415a4f944c7
afmurillo97 <afmurillo97@gmail.com>
2022-05-24 19:45:27 -0500

    props added and conect the database

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,9 @@ import Image from "next/image";
 import Featured from "../components/Featured";
 import styles from "../styles/Home.module.css";
 import DishesList from "../components/DishesList";
+import axios from "axios";
 
-export default function Home() {
+export default function Home({ dishesList }) {
   return (
     <div className={styles.container}>
       <Head>
@@ -13,7 +14,16 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Featured />
-      <DishesList />
+      <DishesList dishesList={dishesList} />
     </div>
   );
 }
+
+export const getServerSideProps = async () => {
+  const res = await axios.get("http://localhost:3000/api/products");
+  return {
+    props: {
+      dishesList: res.data,
+    },
+  };
+};
