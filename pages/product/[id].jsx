746ed8d4860ce1c9bfe35ed7bf613e08f886bae8
afmurillo97 <afmurillo97@gmail.com>
2022-05-26 10:58:02 -0500

    add a handleClick function

--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -2,12 +2,15 @@ import styles from '../../styles/Product.module.css'
 import Image from 'next/image'
 import { useState } from 'react'
 import axios from 'axios'
+import { useDispatch } from 'react-redux';
+import { addProduct } from "../../redux/cartSlice";
 
 const Product = ({ dish }) => {
-        const [price, setPrice] = useState(dish.prices[0])
-        const [size, setSize] = useState(0)
-        const [quantity, setQuantity] = useState(1)
-        const [extras, setExtras] = useState([])
+        const [price, setPrice] = useState(dish.prices[0]);
+        const [size, setSize] = useState(0);
+        const [quantity, setQuantity] = useState(1);
+        const [extras, setExtras] = useState([]);
+        const dispatch = useDispatch();
 
     const changePrice = (number) => {
         setPrice(price + number)
@@ -29,6 +32,11 @@ const Product = ({ dish }) => {
             changePrice(-option.price)
             setExtras(extras.filter(extra => extra._id !== option._id ))
         }
+    }
+
+
+    const handleClick = () => {
+        dispatch(addProduct({...dish, extras, price, quantity}))
     }
 
     return <div className={styles.container}>
@@ -73,7 +81,7 @@ const Product = ({ dish }) => {
             </div>
             <div className={styles.add}>
                 <input onChange={(e) => setQuantity(e.target.value)} type="number" defaultValue={1} className={styles.quantity}/>
-                <button className={styles.button}>Add to Cart</button>
+                <button className={styles.button} onClick={handleClick}>Add to Cart</button>
             </div>
         </div>
     </div>
