ade0b649eb06def9ffd2648d0f60d9e56db2877f
afmurillo97 <afmurillo97@gmail.com>
2022-05-26 11:20:15 -0500

    a functionality for add a new dish in the cart

--- pages/cart.jsx
+++ pages/cart.jsx
@@ -1,8 +1,11 @@
 import styles from '../styles/Cart.module.css'
 import Image from 'next/image'
+import { useDispatch, useSelector } from "react-redux";
 
 
 const Cart = () => {
+    const dispatch = useDispatch();
+    const cart = useSelector((state) => state.cart);
     return (
         <div className={styles.container}>
             <div className={styles.left}>
@@ -15,66 +18,33 @@ const Cart = () => {
                         <th>Quantity</th>
                         <th>Total</th>
                     </tr>
-                    <tr className={styles.tr}>
-                        <td>
-                            <div className={styles.imgContainer}>
-                                <Image src='/img/dish3.png' layout='fill' alt='' objectFit='cover'/>
-                            </div>
-                        </td>
-                        <td>
-                            <span className={styles.name}>FELIPE</span>
-                        </td>
-                        <td>
-                            <span className={styles.extras}>
-                                Double ingredient, spicy sauce
-                            </span>
-                        </td>
-                        <td>
-                            <span className={styles.price}>$19.90</span>
-                        </td>
-                        <td>
-                            <span className={styles.quantity}>2</span>
-                        </td>
-                        <td>
-                            <span className={styles.total}>$39.80</span>
-                        </td>
-                    </tr>
+                    {cart.products.map((product) => (
+                        <tr className={styles.tr} key={product._id}>
+                            <td>
+                                <div className={styles.imgContainer}>
+                                    <Image src='/img/dish3.png' layout='fill' alt='' objectFit='cover'/>
+                                </div>
+                            </td>
+                            <td>
+                                <span className={styles.name}>FELIPE</span>
+                            </td>
+                            <td>
+                                <span className={styles.extras}>
+                                    Double ingredient, spicy sauce
+                                </span>
+                            </td>
+                            <td>
+                                <span className={styles.price}>$19.90</span>
+                            </td>
+                            <td>
+                                <span className={styles.quantity}>2</span>
+                            </td>
+                            <td>
+                                <span className={styles.total}>$39.80</span>
+                            </td>
+                        </tr>
+                    ))}
                 </table>
-                <table className={styles.table}>
-                    <tr className={styles.trTitle}>
-                        <th>Product</th>
-                        <th>Name</th>
-                        <th>Extras</th>
-                        <th>Price</th>
-                        <th>Quantity</th>
-                        <th>Total</th>
-                    </tr>
-                    <tr className={styles.tr}>
-                        <td>
-                            <div className={styles.imgContainer}>
-                                <Image src='/img/dish3.png' layout='fill' alt='' objectFit='cover'/>
-                            </div>
-                        </td>
-                        <td>
-                            <span className={styles.name}>FELIPE</span>
-                        </td>
-                        <td>
-                            <span className={styles.extras}>
-                                Double ingredient, spicy sauce
-                            </span>
-                        </td>
-                        <td>
-                            <span className={styles.price}>$19.90</span>
-                        </td>
-                        <td>
-                            <span className={styles.quantity}>2</span>
-                        </td>
-                        <td>
-                            <span className={styles.total}>$39.80</span>
-                        </td>
-                    </tr>
-                </table>
-                
             </div>
             <div className={styles.right}>
                 <div className={styles.wrapper}>
